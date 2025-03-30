2a84dbfa9b92190dddb77cf6dce1176b3194f827
Diogo Martins de Assis <me@diogomartinsdeassis.com.br>
2025-03-30 17:16:54 -0300

    feat: setup mongodb and rabbitmq client connection

--- gateway/server.py
+++ gateway/server.py
@@ -1,1 +1,18 @@
-print("Hello, World!")
+import os, gridfs, pika
+
+from dotenv import load_dotenv
+from flask import Flask, request
+from flask_pymongo import PyMongo
+
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI_CONNECTION")
+
+mongo = PyMongo(app)
+fs = gridfs.GridFS(mongo.db)
+
+connParameters = os.environ.get("RABBITMQ_CONNECTION_PARAMETERS")
+connection = pika.BlockingConnection(pika.ConnectionParameters(connParameters))
+channel = connection.channel()
