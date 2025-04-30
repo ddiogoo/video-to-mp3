dcc04b75b83722176c8bd2d14bb2e83d4bd75e10
Diogo Martins de Assis <me@diogomartinsdeassis.com.br>
2025-04-29 21:25:39 -0300

    refactor: remove extra modules from auth project

--- auth/server.py
+++ auth/server.py
@@ -1,96 +1,66 @@
-"""
-This python file is the server that will be used to authenticate the user.
-"""
-
-import jwt, os
-
-from dotenv import load_dotenv
+import jwt, datetime, os
 from flask import Flask, request
-from database.database import User, db
-from utils.jwt import create_jwt
+from flask_mysqldb import MySQL
+
+server = Flask(__name__)
+mysql = MySQL(server)
+
+server.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
+server.config["MYSQL_USER"] = os.environ.get("MYSQL_USER")
+server.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD")
+server.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
+server.config["MYSQL_PORT"] = int(os.environ.get("MYSQL_PORT"))
 
 
-load_dotenv()
-
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URI_CONNECTION")
-db.init_app(app)
-
-
-@app.route("/register", methods=["POST"])
-def register():
-    """
-    Register the user.
-
-    Returns:
-        str, int: The success message (Or an error message) and the status code.
-    """
-    user = request.get_json()
-    if not user:
-        return "invalid credentials", 400
-    
-    newUser = User(**user)
-    try:
-        db.session.add(newUser)
-        db.session.commit()
-        return "User created", 201
-    except:
-        db.session.rollback()
-        return "User already exists", 400
-
-
-@app.route("/login", methods=["POST"])
+@server.route("/login", methods=["POST"])
 def login():
-    """
-    Login the user.
-
-    Returns:
-        str, int: The JWT token (Or an error message) and the status code.
-    """
     auth = request.authorization
     if not auth:
-        return "missing credentials", 400
-    
-    user = db.session.query(User).filter(User.email == auth.username).first()
-    if not user:
-        return "User not found", 404
-    
-    if auth.username != user.email or auth.password != user.password:
+        return "missing credentials", 401
+    cur = mysql.connection.cursor()
+    res = cur.execute(
+        "SELECT email, password FROM user WHERE email=%s", (auth.username,)
+    )
+    if res > 0:
+        user_row = cur.fetchone()
+        email = user_row[0]
+        password = user_row[1]
+        if auth.username != email or auth.password != password:
+            return "invalid credentials", 401
+        else:
+            return createJWT(auth.username, os.environ.get("JWT_SECRET"), True)
+    else:
         return "invalid credentials", 401
-    return create_jwt(user.email, os.environ.get('JWT_SECRET'), True)
 
 
-@app.route("/validate", methods=["POST"])
+@server.route("/validate", methods=["POST"])
 def validate():
-    """
-    Validate the user.
-
-    Returns:
-        str, int: The success message (Or an error message) and the status code.
-    """
-    encode_token = request.headers["Authorization"]
-    if not encode_token:
-        return "missing token", 400
-    encode_token = encode_token.split(" ")[1]
+    encoded_jwt = request.headers["Authorization"]
+    if not encoded_jwt:
+        return "missing credentials", 401
+    encoded_jwt = encoded_jwt.split(" ")[1]
     try:
-        jwt.decode(encode_token, os.environ.get("JWT_SECRET"), algorithms=["HS256"])
-    except jwt.ExpiredSignatureError:
-        return "expired token", 401
-    except jwt.InvalidTokenError:
-        return "invalid token", 401
-    return "valid token", 200
+        decoded = jwt.decode(
+            encoded_jwt, os.environ.get("JWT_SECRET"), algorithms=["HS256"]
+        )
+    except:
+        return "not authorized", 403
+    return decoded, 200
+
+
+def createJWT(username, secret, authz):
+    return jwt.encode(
+        {
+            "username": username,
+            "exp": datetime.datetime.now(tz=datetime.timezone.utc)
+            + datetime.timedelta(days=1),
+            "iat": datetime.datetime.utcnow(),
+            "admin": authz,
+        },
+        secret,
+        algorithm="HS256",
+    )
 
 
 if __name__ == "__main__":
-    """
-    Run the server.
-
-    Raises:
-        e: An exception if the db cannot create all the tables.
-    """
-    try:
-        with app.app_context():
-            db.create_all()
-    except Exception as e:
-        raise e.args
-    app.run(host="0.0.0.0", port=5000)
+    server.run(host="0.0.0.0", port=5000)
